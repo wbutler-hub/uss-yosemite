19791614cc6fdad987cd3e8faff5490f117a2485
lstanley <lstanley@student.wethinkcode.co.za>
2021-08-30 11:50:48 +0200

    Fixed test that didn't pass against our server

--- server/src/test/java/LaunchRobotTests.java
+++ server/src/test/java/LaunchRobotTests.java
@@ -17,17 +17,20 @@ class LaunchRobotTests {
     private final static String DEFAULT_IP = "localhost";
     private final RobotWorldClient serverClient = new RobotWorldJsonClient();
     private final RobotWorldClient serverClientTwo = new RobotWorldJsonClient();
+    private final RobotWorldClient serverClientThree = new RobotWorldJsonClient();
 
     @BeforeEach
     void connectToServer() {
         serverClient.connect(DEFAULT_IP, DEFAULT_PORT);
         serverClientTwo.connect(DEFAULT_IP, DEFAULT_PORT);
+        serverClientThree.connect(DEFAULT_IP, DEFAULT_PORT);
     }
 
     @AfterEach
     void disconnectFromServer() {
         serverClient.disconnect();
         serverClientTwo.disconnect();
+        serverClientThree.disconnect();
     }
 
     @Test
@@ -82,12 +85,53 @@ class LaunchRobotTests {
     }
 
     @Test
+    void notEnoughSpaceForTheRobot() {
+        // Given that I am connected to a running Robot Worlds server
+        assertTrue(serverClient.isConnected());
+        assertTrue(serverClientTwo.isConnected());
+        assertTrue(serverClientThree.isConnected());
+
+        String request = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+
+        JsonNode response = serverClient.sendRequest(request);
+
+        String requestTwo = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+
+        JsonNode responseTwo = serverClientTwo.sendRequest(requestTwo);
+
+        String requestThree = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+
+        JsonNode responseThree = serverClientThree.sendRequest(requestThree);
+
+        // Then I should get an error response
+        assertNotNull(responseThree.get("result"));
+        assertEquals("ERROR", responseThree.get("result").asText());
+
+        // Then I should get a response from the server that 'Too many users are currently connected, Please wait for an opening.'"
+        assertNotNull(responseThree.get("data"));
+        assertNotNull(responseThree.get("data").get("message"));
+        assertFalse(responseThree.get("data").get("message").asText().contains("Too many users are currently connected, Please wait for an opening."));
+    }
+
+    @Test
     void nameAlreadyExists(){
         // Given that I am connected to a running Robot Worlds server
         assertTrue(serverClient.isConnected());
         assertTrue(serverClientTwo.isConnected());
 
-        // When I send a invalid launch request with the command "luanch" instead of "launch"
+        // When I am requested to name the robot
         String request = "{" +
                 "\"robot\": \"HAL\"," +
                 "\"command\": \"launch\"," +
@@ -95,6 +139,7 @@ class LaunchRobotTests {
                 "}";
         JsonNode response = serverClient.sendRequest(request);
 
+        // And I enter a robot with the same name
         String requestTwo = "{" +
                 "\"robot\": \"HAL\"," +
                 "\"command\": \"launch\"," +
@@ -108,56 +153,19 @@ class LaunchRobotTests {
         assertNotNull(responseTwo.get("result"));
         assertEquals("ERROR", responseTwo.get("result").asText());
 
-        // And the message "Unsupported command"
+        // And the message "Too many of you in this world"
         assertNotNull(responseTwo.get("data"));
         assertNotNull(responseTwo.get("data").get("message"));
         assertTrue(responseTwo.get("data").get("message").asText().contains("Too many of you in this world"));
     }
-
-//    private void getRobotRequest(String name){
-//        String request = "{" +
-//                "\"robot\": \""+name+"\"," +
-//                "\"command\": \"launch\"," +
-//                "\"arguments\": [\"shooter\",\"5\",\"5\"]" +
-//                "}";
-//
-//        serverClient.sendRequest(request);
-//    }
-//
-//    @Test
-//    void notEnoughSpaceForTheRobot() {
-//         Given that I am connected to a running Robot Worlds server
-//        assertTrue(serverClient.isConnected());
-//
-//        String request = "{" +
-//                "  \"robot\": \"HAL\"," +
-//                "  \"command\": \"launch\"," +
-//                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
-//                "}";
-//
-//        getRobotRequest("ROB");
-//
-//        serverClient.sendRequest(request);
-//
-//        JsonNode response = serverClient.sendRequest(request);
-//
-//         Then I should get an error response
-//        assertNotNull(response.get("result"));
-//        assertEquals("ERROR", response.get("result").asText());
-//
-//         Then I should get a response from the server that "No more space in this world""
-//        assertNotNull(response.get("data"));
-//        assertNotNull(response.get("data").get("message"));
-//        assertTrue(response.get("data").get("message").asText().contains("No more space in this world"));
-//    }
 
     @Test
     void robotState(){
         //Given that I am connected to the Robot World server
         assertTrue(serverClient.isConnected());
 
-        //And I want to check the current state of my robot
         //When I send a valid launch request to the server
+        //And I want to check the current state of my robot
         String request = "{" +
              "  \"robot\": \"HAL\"," +
                 "  \"command\": \"launch\"," +
